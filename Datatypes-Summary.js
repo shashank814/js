776121d09ca089644e847cd68f8b609751b67d87
Shashank Singh <sshingh456@gmail.com>
2024-11-19 10:15:55 +0000

    Datatype-Summary.js

---
+++ Datatypes-Summary.js
@@ -0,0 +1,35 @@
+// Primitive : 7 Types
+
+// String, Number, Boolean, Null , Undeifined, Symbol, BigInt
+
+const score = 100
+const score = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+Let userEmail;
+
+const id = Symbol('123')
+const anothetId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 234567853545366n
+
+// Reference (Non-Primitive) : Array, Objects, Functions
+
+const heros = ["shaktiman", "nagaraj", "doga"];
+let myObj {
+    name : "roshan"
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeOf bigNumber)
+
+console.log(typeOf outSideTemp)
+
+console.log(typeOf myFunction)
