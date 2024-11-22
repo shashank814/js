3b52c4bff25b3d6fc6ed6156b9fc04025b0467a5
Shashank Singh <sshingh456@gmail.com>
2024-11-22 09:53:15 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+// ************ NUMS **************
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);     // 
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966         // 123.44  // 1234.45
+
+// console.log(otherNumber.toPrecision(3)); 
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  ************* MATHS ***************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(2,6,3,4,5));
+// console.log(Math.max(2,6,3,4,5));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + 1)
