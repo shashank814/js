1e9ced9495dca8a0aed334604acb11d3147e68d2
Shashank Singh <sshingh456@gmail.com>
2024-11-30 06:33:06 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -33,4 +33,36 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage())
+// console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(val1, val2, ...num1) {      // ... -> rest operator
+           return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "roshan",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+/*
+ handleObject({
+    username: "sam",
+    price:199
+})
+    */
+
+const myNewArray = [200, 400, 500, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 2000]));
