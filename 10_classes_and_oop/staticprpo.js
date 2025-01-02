1cead0e2fcb8d69fa7dcf53997ce8710cf4ef914
Shashank Singh <sshingh456@gmail.com>
2025-01-02 07:37:05 +0000

    static properties

---
+++ 10_classes_and_oop/staticprpo.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const roshan = new User("roshan")
+// console.log(roshan.createId());
+
+class Teacher extends User {
+    constructor(username, email) {
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+//iphone.logMe()
+
+console.log(iphone.createId())
